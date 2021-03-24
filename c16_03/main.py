429c019c234c307df1de98514e45d6f12274e20d
Zakaria Mehdaoui <zmehdaoui@yahoo.fr>
2021-03-24 13:33:36 +0000

    Coorection/execution du script

--- c16_03/main.py
+++ c16_03/main.py
@@ -1,7 +1,17 @@
 #Zakaria Mehdaoui G26
 #Question 4
+note1 = ('eleve1', 'math', 13)
+note2 = ('eleve1', 'physique', 15)
+note3 = ('eleve1', 'math', 13)
+note4 = ('eleve1', 'eco', 12)
+note5 = ('eleve1', 'eco', 13)
+note6 = ('eleve1', 'math', 12)
+note7 = ('eleve2', 'math', 13)
+note8 = ('eleve2', 'math', 14)
+
+notes = [note1, note2, note3, note4, note5, note6,note7,note8]
 #a)
-x1 = (note[2]+notes[1][2]+notes[2][2]+notes[3][2]+notes[4][2]+notes[5][2])/6
+x1 = (notes[0][2]+notes[1][2]+notes[2][2]+notes[3][2]+notes[4][2]+notes[5][2])/6
 print("La moyenne de l'élève 1 est de ", x1)
 #4)b
 xlev1_math = (notes[0][2]+notes[2][2]+notes[5][2])/3
